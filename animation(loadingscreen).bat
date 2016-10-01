6b8c304debaf65050f90404b235ba9b5b30e5582
Keishon Smith <keishon104@gmail.com>
2016-10-01 00:26:24 -0400

    Loading Screen Animation
    Simple loading animation using batch programming.

---
+++ animation(loadingscreen).bat
@@ -0,0 +1,36 @@
+@echo off
+cls 
+echo Loading[.      ]
+ping localhost -n 2 >nul
+cls
+@echo off
+cls 
+echo Loading[..     ]
+ping localhost -n 2 >nul
+cls
+@echo off
+cls 
+echo Loading[...    ]
+ping localhost -n 2 >nul
+cls
+@echo off
+cls 
+echo Loading[....   ]
+ping localhost -n 2 >nul
+cls
+@echo off
+cls 
+echo Loading[.....  ]
+ping localhost -n 2 >nul
+cls
+@echo off
+cls 
+echo Loading[...... ]
+ping localhost -n 2 >nul
+cls
+@echo off
+cls 
+echo Loading[.......]
+ping localhost -n 2 >nul
+cls
+
